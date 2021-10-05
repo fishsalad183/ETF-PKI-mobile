3772b3539cb26b38363badf6d97817b1f03b9a4d
v <vbogos3@gmail.com>
2021-10-05 21:46:33 +0200

    Removed unnecessary commented out code

--- app/src/main/java/com/pki/medenjaci/CartAdapter.kt
+++ app/src/main/java/com/pki/medenjaci/CartAdapter.kt
@@ -34,37 +34,6 @@ class CartAdapter(private val cart: Cart, private val cartItemClickListener: (Ca
         holder.bind(cart[position])
     }
 
-//    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val amount: TextView = itemView.findViewById(R.id.lbl_cartitem_amount)
-//        val image: ImageView = itemView.findViewById(R.id.img_cartitem)
-//        val name: TextView = itemView.findViewById(R.id.lbl_cartitem_name)
-//        val price: TextView = itemView.findViewById(R.id.lbl_cartitem_price)
-//        val actionButtonRemove: ImageView = itemView.findViewById(R.id.btn_cartitem_remove)
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_cart, parent, false)
-//        return ViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val cartItem = cart[position]
-//        with(holder) {
-//            amount.text = amount.context.getString(R.string.amount_number, cartItem.amount)
-//            image.setImageResource(cartItem.product.imgResourceID)
-//            name.text = cartItem.product.name
-//            price.text = price.context.getString(
-//                R.string.price_short,
-//                cartItem.amount * cartItem.priceIncludingDiscounts
-//            )
-//            actionButtonRemove.setOnClickListener {
-//                cart.removeAt(position)
-//                notifyItemRemoved(position)
-//                renderOrderElements()
-//            }
-//        }
-//    }
-
     override fun getItemCount() = cart.size
 
 }
